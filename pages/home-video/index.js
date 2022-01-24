e7641d23e347ebcf567fda7e109906d83e8ad57c
rmb <335147451@qq.com>
2022-01-24 22:17:44 +0800

    动态调整视频页面swiper的高度，方便显示出当前item的所有内容（只能设置swiper才生效） 动态决定当前类别样式、实现了点击顶上类别tab和滑动都能切换类别的功能 记录了滚动高度，实现不同类别切换时保持当前原有滚动高度的效果 每个类别的数据在第一次切换到该类别后单独加载，且分别单独实现上拉加载更多和下拉刷新的功能互不影响

--- pages/home-video/index.js
+++ pages/home-video/index.js
@@ -1,38 +1,123 @@
 import { getTopMV } from "../../service/getMVData";
-const gData = getApp().globalData;
+import getSelectorRect from "../../utils/getSelectorRect";
 Page({
   data: {
-    topMVs: [],
-    hasMore: true,
-    contentHeight:
-      gData.screenHeight - gData.statusBarHeight - gData.navBarHeight,
+    currentIndex: 0,
+    swiperItemsInfo: [],
+    contentHeights: [],
+    scrollTops: [],
   },
-  async getTopMVData(offset) {
-    // 请求不是第一段数据时，要判断是否越界
-    if (offset && !this.data.hasMore) return;
-    const res = await getTopMV(offset);
-    // 不越界或者是第一段，则分情况设置数据
-    if (offset === 0) {
-      this.setData({
-        topMVs: res.data,
-        hasMore: true,
-      });
-    } else {
-      this.setData({
-        topMVs: [...this.data.topMVs, ...res.data],
-        hasMore: res.hasMore,
-      });
-    }
-    console.log(res);
-  },
+
+  // 自带回调
   onLoad(options) {
-    this.getTopMVData(0);
+    this.initialSwiperItemsInfo();
+    this.setOffsetThenGetData(true);
   },
+  // 上拉下拉到位监听
   onReachBottom() {
-    this.getTopMVData(this.data.topMVs.length);
+    this.setOffsetThenGetData(false);
   },
   onPullDownRefresh() {
-    this.getTopMVData(0);
+    this.setOffsetThenGetData(true);
     wx.stopPullDownRefresh();
+  },
+
+  //自定义监听
+  // 两个监听基本相同，都是切换swiperItem的回调
+  handleTabClick(e) {
+    // 记录切换前下标的滚动高度
+    this.setScrollTop(this.data.currentIndex);
+    const currentIndex = e.currentTarget.dataset.index;
+    this.setData({ currentIndex });
+    // 第一次则请求数据，否则滚到上次高度
+    if (this.data.swiperItemsInfo[currentIndex].isFirst) {
+      this.setOffsetThenGetData(true);
+    } else {
+      wx.pageScrollTo({
+        scrollTop: 53 - this.data.scrollTops[currentIndex],
+        duration: 0,
+      });
+    }
+  },
+  handleItemChange(e) {
+    this.setScrollTop(this.data.currentIndex);
+    const currentIndex = e.detail.current;
+    this.setData({ currentIndex });
+    if (this.data.swiperItemsInfo[currentIndex].isFirst) {
+      this.setOffsetThenGetData(true);
+    } else {
+      wx.pageScrollTo({
+        scrollTop: 53 - this.data.scrollTops[currentIndex],
+        duration: 0,
+      });
+    }
+  },
+
+  // 功能函数
+  // 初始化swiperItemsInfo
+  initialSwiperItemsInfo() {
+    const areas = ["全部", "内地", "港台", "欧美", "日本", "韩国"];
+    const swiperItemsInfo = areas.map((item, index) => {
+      return {
+        area: item,
+        index,
+        offset: 0,
+        // 是否第一次请求
+        isFirst: true,
+      };
+    });
+    this.setData({ swiperItemsInfo });
+  },
+
+  // 设置offset并获取数据
+  setOffsetThenGetData(isInitial) {
+    const swiperItemsInfo = this.data.swiperItemsInfo;
+    const currentIndex = this.data.currentIndex;
+    // 检测是否第一次请求
+    if (swiperItemsInfo[currentIndex].isFirst) {
+      swiperItemsInfo[currentIndex].isFirst = false;
+    }
+    swiperItemsInfo[currentIndex].offset = isInitial
+      ? 0
+      : swiperItemsInfo[currentIndex].topMVs.length;
+    this.setData({ swiperItemsInfo });
+    this.getTopMVData(swiperItemsInfo[currentIndex]);
+  },
+
+  // 获取MV数据
+  async getTopMVData(itemInfo) {
+    // 请求不是第一段数据时，要判断是否越界
+    if (itemInfo.offset !== 0 && !itemInfo.hasMore) return;
+    const res = await getTopMV(itemInfo);
+    // 不越界或者是第一段，则分情况设置数据
+    if (itemInfo.offset === 0) {
+      itemInfo.topMVs = res.data;
+      itemInfo.hasMore = true;
+    } else {
+      itemInfo.topMVs = [...itemInfo.topMVs, ...res.data];
+      itemInfo.hasMore = res.hasMore;
+    }
+    // 修改data中的swiperItemsInfo后，更新组件高度
+    const swiperItemsInfo = this.data.swiperItemsInfo;
+    swiperItemsInfo[this.data.currentIndex] = itemInfo;
+    this.setData({ swiperItemsInfo });
+    this.updateCurrentItemHeight();
+  },
+
+  // 更新组件高度
+  updateCurrentItemHeight() {
+    // 更新高度和当前滚动位置
+    const currentIndex = this.data.currentIndex;
+    const contentHeights = this.data.contentHeights;
+    contentHeights[currentIndex] =
+      75 * this.data.swiperItemsInfo[currentIndex].topMVs.length;
+    this.setData({ contentHeights });
+  },
+  // 记录滚动高度
+  async setScrollTop(currentIndex) {
+    const res = await getSelectorRect(".video-list");
+    const scrollTops = this.data.scrollTops;
+    scrollTops[currentIndex] = res.top;
+    this.setData({ scrollTops });
   },
 });
--- service/getMVData.js
+++ service/getMVData.js
@@ -1,17 +1,12 @@
 import sfRequest from "./index.js";
 // 封装所有MV相关网络请求
 // 中间层架构，订制一个获取最热MV的请求，最少只需要offset一个参数
-export function getTopMV(
-  offset,
-  limit = 20,
-  area = "全部",
-  order = "上升最快"
-) {
+export function getTopMV(itemInfo) {
+  let { offset, limit, area } = itemInfo;
+  if (!limit) limit = 20;
   return sfRequest.get("/mv/all", {
     offset,
     limit,
-    // 排序,可选值为上升最快,最热,最新,不填则为上升最快
-    order,
     // 地区,可选值为全部,内地,港台,欧美,日本,韩国,不填则为全部
     area,
   });
